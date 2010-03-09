7d9f7555459ad89d6589306af0bb6b26102489a1
xjanssen <>
2010-03-09 08:47:05 +0000

    add Makefile + nain program

---
+++ ChPartAna/macro/mcollectionPlotter.C
@@ -0,0 +1,10 @@
+#include "TString.h" 
+
+void collectionPlotter(int = 10 , double = 0.9 , double = 0.2 , TString = "NONE" , int = 5000 );
+
+
+int main()
+
+{
+  collectionPlotter(10,0.9,0.4,"NONE",1000000);
+}
