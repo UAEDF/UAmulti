2265ebcd4be3b4e61803bb0c222accea94218067
rougny <>
2010-04-09 17:23:36 +0000

    Corrected last bin being huge

--- ChPartAna/scripts/getGoodBinning.C
+++ ChPartAna/scripts/getGoodBinning.C
@@ -48,32 +48,65 @@ void getGoodBinning(TString filename){
     TH1F* hist = (TH1F*) file->Get(histname.str().c_str());
     
     
-    
+    int lastwidth = 1;
     bool first = false;
     for(int i=1 ; i<=hist->GetNbinsX() ; ++i){
-      int binwidth=hist->GetBinWidth(i);
-      if(hist->GetBinContent(i)<20 && hist->GetBinContent(i)!=0 && !first && binwidth==1)
+      int binwidth = int(hist->GetBinWidth(i));
+      
+      //making first bins with width 1
+      if(hist->GetBinContent(i)<20 && hist->GetBinContent(i)!=0 && !first && binwidth==1){
         cout<<"    tmp = makeBins("<<i-1<<",-0.5,"<<double(i)-1.5<<");"<<endl;
-	
+        first=true;
+      }
+      
+      //making first bins with width=1,then others with width!=1 && stat>20
       if(hist->GetBinContent(i)>=20 && binwidth!=1 && !first){
         cout<<"    tmp = makeBins("<<i-1<<",-0.5,"<<double(i)-1.5<<");"<<endl;
-	  first = true;
+	first = true;
       }
-      if(hist->GetBinContent(i)>=20 && binwidth!=1)
+      
+      if(hist->GetBinContent(i)>=20 && first)
         cout<<"    addBins(1,"<<binwidth<<",tmp);"<<endl;
       
-      if(hist->GetBinContent(i)<20 && hist->GetBinContent(i)!=0){
+      //making bins with stat<20
+      if(hist->GetBinContent(i)<20){
         first = true;
         double n = hist->GetBinContent(i);
 	int j = 0;
+	bool useLastBin = false;
+	int nnull = 0;
         for(j=i+1;j<=hist->GetNbinsX() ; ++j){
+	  if(hist->GetBinContent(j)==0) ++nnull;
           n+=hist->GetBinContent(j);
-	  binwidth += hist->GetBinWidth(j);
+	  binwidth += int(hist->GetBinWidth(j));
+	  
+	  if(nnull>5){useLastBin=true;break;}
 	  if(n>20) break;
         }
-        cout<<"    addBins(1,"<<j-i+1<<",tmp);"<<endl;
-	i=j;
+	
+	if(!useLastBin && binwidth<=10){
+          cout<<"    addBins(1,"<<binwidth<<",tmp);"<<endl;
+	  i=j;
+	}
+	
+	if(!useLastBin && binwidth>10){
+          cout<<"    addBins(1,10,tmp);"<<endl;
+	  binwidth=10;
+	  i+=10;
+	}
+	if(useLastBin){
+	  if(lastwidth<=5)
+	    binwidth=5;
+	  else
+	    binwidth=lastwidth;
+	    
+	  int nbin = int((120-i)/binwidth);
+	  cout<<"    addBins("<<nbin+1<<","<<binwidth<<",tmp);"<<endl;
+	  break;
+	}
+	
       }
+      lastwidth = binwidth;
     }
     cout<<"  }"<<endl;
   }//end of loop over acceptance map
