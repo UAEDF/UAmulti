59ea68914b75f371e7b6fdd44a44933e13b9f3f3
rougny <>
2010-04-30 14:04:28 +0000

    added single nbd fit , and drawing of the 2 nbds in the double nbd fit.

--- ChPartAna/scripts/fitting.C
+++ ChPartAna/scripts/fitting.C
@@ -14,23 +14,23 @@
   cout<<"Scaling nbd by : "<<nch_corrected->Integral()<<endl<<endl;
   
   // ONE NBD
-  /*TF1* nbd = new TF1("nbd",nbdfunc,3,maxfit,3);
-  nbd->SetParNames("nmean","k","scalefactor");
-  nbd->FixParameter(3,nch_corrected->Integral());
-  nbd->SetParameters(14,9);
-  nbd->SetParLimits(0,3,35);
-  nbd->SetParLimits(1,1,12);*/
+  TF1* singlenbd = new TF1("singlenbd",nbdfunc,6,20,3);
+  singlenbd->SetParNames("nmean","k","scalefactor");
+  singlenbd->FixParameter(3,nch_corrected->Integral());
+  singlenbd->SetParameters(10,5);
+  singlenbd->SetParLimits(0,3,20);
+  singlenbd->SetParLimits(1,1,30);
   
   //TWO NBDs
-  TF1* nbd = new TF1("nbd",nbdfunc2,1,maxfit,6);
+  TF1* nbd = new TF1("nbd",nbdfunc2,0.5,maxfit,6);
   nbd->SetParNames("alpha","nmean1","k1","nmean2","k2","scalefactor");
-  nbd->SetParameters(0.72,12,3,30,7,nch_corrected->Integral());
+  nbd->SetParameters(0.72,12,3,nch_corrected->GetMean()+15,7,nch_corrected->Integral());
   //nbd->FixParameter(5,nch_corrected->Integral());
   nbd->SetParLimits(0,0,1);
-  nbd->SetParLimits(1,3,35);
-  nbd->SetParLimits(2,1,12);
-  nbd->SetParLimits(3,10,70);
-  nbd->SetParLimits(4,3,80);
+  nbd->SetParLimits(1,0.1,40);
+  nbd->SetParLimits(2,0.5,20);
+  nbd->SetParLimits(3,30,90);
+  nbd->SetParLimits(4,0.5,80);
   
   
   //TWO NBDs bis
@@ -48,8 +48,7 @@
   //THREE NBDs
   /*TF1* nbd = new TF1("nbd",nbdfunc3,1,maxfit,9);
   nbd->SetParNames("alpha1","nmean1","k1","alpha2","nmean2","k2","nmean3","k3","scalefactor");
-  nbd->FixParameter(4,nch_corrected->Integral());
-  nbd->SetParameters(0.70,5,10,0.25,24,10,50,20);
+  nbd->SetParameters(0.70,5,10,0.25,24,10,50,20,nch_corrected->Integral());
   nbd->SetParLimits(0,0.5,1);
   nbd->SetParLimits(1,3,35);
   nbd->SetParLimits(2,1,12);
@@ -70,11 +69,24 @@
   nbd->SetParLimits(2,1,12);
   nbd->SetParLimits(3,0.01,0.5);*/
   
+  //nch_corrected->Scale(1./nch_corrected->Integral());
+    
+  singlenbd->SetRange(0.5,20);
+  nch_corrected->Fit("singlenbd","RO0");
+  singlenbd->SetRange(-0.5,120);
+  nbd->GetYaxis()->SetRangeUser(0.01,singlenbd->GetMaximum()*1.1);
+  singlenbd->SetLineWidth(1);
+  singlenbd->SetLineColor(kBlue);
+  singlenbd->Draw("same");
   
+  //nbd->FixParameter(1,singlenbd->GetParameter(0));
+  //nbd->FixParameter(2,singlenbd->GetParameter(1));
   
   nch_corrected->Fit("nbd","RO0");
-  nbd->SetRange(0,80);
+  //nbd->SetRange(0,80);
   nbd->Draw("same");
+  
+  
   
   TCanvas* c_fit = new TCanvas("c_fit","c_fit",1460,10,500,500);
   c_fit->SetLeftMargin(0.17);
@@ -91,6 +103,28 @@
   nbd->SetLineWidth(1);
   nbd->Draw();
   nch_corrected->Draw("same");
+  singlenbd->Draw("same");
+  
+  TF1* nbd_draw1 = new TF1("nbd_draw1",nbdfunc,0.,nbd->GetXmax(),3);
+  TF1* nbd_draw2 = new TF1("nbd_draw2",nbdfunc,0.,nbd->GetXmax(),3);
+  
+  
+  nbd_draw1->SetParameters(nbd->GetParameter(1) , nbd->GetParameter(2) , nbd->GetParameter(0) * nbd->GetParameter(5));
+  //cout<<nbd->GetParameter(0) * nbd->GetParameter(8)<<"  "<<nbd->GetParameter(1)<<"  "<<nbd->GetParameter(2)<<endl;
+  //nbd_draw2->SetParameters(nbd->GetParameter(4) , nbd->GetParameter(5) , (nbd->GetParameter(3)) * nbd->GetParameter(8));
+  nbd_draw2->SetParameters(nbd->GetParameter(3) , nbd->GetParameter(4) , (1-nbd->GetParameter(0)) * nbd->GetParameter(5));
+  
+  
+  nbd_draw1->SetLineWidth(1);
+  nbd_draw2->SetLineWidth(1);
+  nbd_draw1->SetLineStyle(2);
+  nbd_draw2->SetLineStyle(2);
+  nbd_draw1->Draw("same");
+  nbd_draw2->Draw("same");
+  
+  
+  //ua5_multi->SetMarkerStyle(kOpenCircle);
+  //ua5_multi->Draw("P same");
   
   
   TCanvas* c_sigmafit = new TCanvas("c_sigmafit","c_sigmafit");
@@ -113,7 +147,7 @@
 
 
   //Doing an iterative fix of the bin mean for width>2
-  /*TGraphAsymmErrors g_nch_corr_1 = fixBinMean(h2g(nch_corrected),nbd);
+ /* TGraphAsymmErrors g_nch_corr_1 = fixBinMean(h2g(nch_corrected),nbd);
 
   TCanvas* c_fit2 = new TCanvas("c_fit2","c_fit2");
   c_fit2->cd();
