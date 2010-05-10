6f342a6dbecba4cc6cadf720ff98834ef83afb3f
rougny <>
2010-05-10 09:05:08 +0000

    added 402 and 403 to use 7TeV mtx with phojet and D6T

--- ChPartAna/scripts/syst_matrix.C
+++ ChPartAna/scripts/syst_matrix.C
@@ -19,6 +19,44 @@ if(syst==401){
   cout<<"++ END OF SYSTEMATIC ++"<<endl;
 }
 
+if(syst==402){
+  ostringstream ext("");
+  ext<<"binningE"<<E;
+  TString mcfiletmp = fileManager(2,30,7.0,iTr,0,0,ext.str(),filedir);
+  TFile* mctmp = TFile::Open(mcfiletmp,"READ");
+  if(mctmp==0){
+    cout<<"!! WARNING : the following file is not present. You nead to create it. Exiting now !"<<endl;
+    cout<<mcfiletmp<<endl;
+    return;
+  }
+  matrixhist = (TH2F*) mctmp->Get(dir+st("MatrixPlots_evtSel",acc)+st("/nch_matrix_evtSel",acc));
+  
+  
+  cout<<"++ SYSTEMATIC #"<<syst<<" ++"<<endl;
+  cout<<"   ===> The MC matrix is now @ an energy of 7 TeV using D6T tune"<<endl;
+  cout<<"   ===> taken from file "<<mcfiletmp<<endl;
+  cout<<"++ END OF SYSTEMATIC ++"<<endl;
+}
+
+if(syst==403){
+  ostringstream ext("");
+  ext<<"binningE"<<E;
+  TString mcfiletmp = fileManager(2,20,7.0,iTr,0,0,ext.str(),filedir);
+  TFile* mctmp = TFile::Open(mcfiletmp,"READ");
+  if(mctmp==0){
+    cout<<"!! WARNING : the following file is not present. You nead to create it. Exiting now !"<<endl;
+    cout<<mcfiletmp<<endl;
+    return;
+  }
+  matrixhist = (TH2F*) mctmp->Get(dir+st("MatrixPlots_evtSel",acc)+st("/nch_matrix_evtSel",acc));
+  
+  
+  cout<<"++ SYSTEMATIC #"<<syst<<" ++"<<endl;
+  cout<<"   ===> The MC matrix is now @ an energy of 7 TeV using phojet tune"<<endl;
+  cout<<"   ===> taken from file "<<mcfiletmp<<endl;
+  cout<<"++ END OF SYSTEMATIC ++"<<endl;
+}
+
 if(syst==410 || syst==411 || syst==412 || syst==413){
   
   TString mcfiletmp = fileManager(2,syst%400,Emc,iTr,0,0,"",filedir);
