37dea7f9ba5be33d5bd37d02b050ae65bd3a545a
rougny <>
2010-06-10 12:34:54 +0000

    changed cosmetic of plots

--- ChPartAna/scripts/knoplot.C
+++ ChPartAna/scripts/knoplot.C
@@ -28,7 +28,7 @@ double  globalEnergy = 0.9;
 #include "../macro/fileManager.C"
 #include "plot.C"
 
-void knoplot(int iBin = 5 , int iSaveFig = 1, float npx = 1000 , float npy = 600){
+void knoplot(int iBin = 5 , int iSaveFig = 0, float npx = 1000 , float npy = 1328){
 
   gROOT->ProcessLine(".x cmsStyleRoot.C");
 
@@ -36,6 +36,8 @@ void knoplot(int iBin = 5 , int iSaveFig = 1, float npx = 1000 , float npy = 600
  
   globalCanvasSizeX = npx; 
   globalCanvasSizeY = npy;
+  
+  globalLegendTextSize = 40;
  
   XaxisTitle = "z=n/<n>";
   YaxisTitle = "#Psi(z)";
@@ -52,20 +54,20 @@ void knoplot(int iBin = 5 , int iSaveFig = 1, float npx = 1000 , float npy = 600
   if ( iBin == 19 ) globalFigBaseName = "kno_eta05_pt500";
 
 
-  globalNorm = 2; 
-  if ( iBin ==  5 )  histoYMin  = 0.000001 ;
-  if ( iBin ==  7 )  histoYMin  = 0.000000001 ;
-  if ( iBin ==  9 )  histoYMin  = 0.000000001 ;
-  if ( iBin == 15 )  histoYMin  = 0.000001 ;
-  if ( iBin == 17 )  histoYMin  = 0.000000001 ;
-  if ( iBin == 19 )  histoYMin  = 0.000000001 ;
+  globalNorm = 0; 
+  if ( iBin ==  5 )  histoYMin  = 0.0003 ;
+  if ( iBin ==  7 )  histoYMin  = 0.000004 ;
+  if ( iBin ==  9 )  histoYMin  = 0.00001 ;
+  if ( iBin == 15 )  histoYMin  = 0.0001 ;
+  if ( iBin == 17 )  histoYMin  = 0.00001 ;
+  if ( iBin == 19 )  histoYMin  = 0.00001 ;
 
-  histoYMax  = 0.3 ;
+  histoYMax  = 1.3 ;
   histoXMin  = -0.5 ;
 
-  if (iBin ==  5 ) histoXMax  = 6. ;
-  if (iBin ==  7 ) histoXMax  = 10.;
-  if (iBin ==  9 ) histoXMax  = 10. ;
+  if (iBin ==  5 ) histoXMax  = 6 ;
+  if (iBin ==  7 ) histoXMax  = 9.;
+  if (iBin ==  9 ) histoXMax  = 9. ;
   if (iBin > 10 ) histoYMax = 1.0; 
   if (iBin == 15 ) histoXMax  = 10.;
   if (iBin == 17 ) histoXMax  = 12 ;
@@ -86,7 +88,9 @@ void knoplot(int iBin = 5 , int iSaveFig = 1, float npx = 1000 , float npy = 600
 //  if ( globalEnergy == 7.0 ) iMc = 31 ;
 
     TString plotdir("");
-    plotdir = "../plots/systv9_bis/";
+    plotdir = "../plots/systv10_undecies2/";
+    //plotdir = "../plots/systv10_ter2/";
+    //plotdir = "../plots/systv10_binning1v2/";
 
 //  if ( globalEnergy == 0.9 ) plotdir = "../plots/finalv9/";
 //  if ( globalEnergy == 2.36) plotdir = "../plots/finalv9/";
@@ -103,7 +107,7 @@ void knoplot(int iBin = 5 , int iSaveFig = 1, float npx = 1000 , float npy = 600
     dataSetHType.push_back(1);
     dataSetStyle.push_back(kOpenCircle);
     dataSetColor.push_back(kBlack);
-    dataSetLegend.push_back("Data 0.9 TeV");
+    dataSetLegend.push_back("CMS 0.9 TeV");
     //dataSetHisto.push_back("unfolding/nch_data_corrected");  
     dataSetHisto.push_back("unfolding/kno_corrected");  
 
@@ -125,7 +129,7 @@ void knoplot(int iBin = 5 , int iSaveFig = 1, float npx = 1000 , float npy = 600
     dataSetHType.push_back(1);
     dataSetStyle.push_back(kOpenSquare);
     dataSetColor.push_back(kBlue);
-    dataSetLegend.push_back("Data 2.36 TeV");
+    dataSetLegend.push_back("CMS 2.36 TeV");
     dataSetHisto.push_back("unfolding/kno_corrected");
 
 
@@ -138,15 +142,13 @@ void knoplot(int iBin = 5 , int iSaveFig = 1, float npx = 1000 , float npy = 600
     dataSetLegend.push_back("NONE");
     dataSetHisto.push_back("unfolding/gkno_corrected_syst");
 
-    plotdir = "../plots/systv10/";
-
     dataSetId.push_back(-1);
     dataSetFile.push_back(fileManager(3,31,7.0,1,0,0,outstr.str(),plotdir));
     dataSetIsMc.push_back(0);
     dataSetHType.push_back(1);
     dataSetStyle.push_back(kOpenTriangleUp);
     dataSetColor.push_back(kRed);
-    dataSetLegend.push_back("Data 7 TeV");
+    dataSetLegend.push_back("CMS 7.0 TeV");
     dataSetHisto.push_back("unfolding/kno_corrected");
 
 
