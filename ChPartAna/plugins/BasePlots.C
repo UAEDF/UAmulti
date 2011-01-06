0d5a93b3ab989cda79889620a03aaf9fcd4d9334
sluyckx <>
2011-01-06 16:05:56 +0000

    fixed bug of arrays initialisation

--- ChPartAna/plugins/BasePlots.C
+++ ChPartAna/plugins/BasePlots.C
@@ -3,7 +3,7 @@
 
 ClassImp(BasePlots)
 
-  Int_t     BasePlots::nch_nbin  = 111;
+  Int_t     BasePlots::nch_nbin  = 301;
   Double_t  BasePlots::nch_xmin  = -0.5;
   Double_t  BasePlots::nch_xmax  = BasePlots::nch_xmin + BasePlots::nch_nbin;
   Double_t* BasePlots::nch_array;
@@ -33,34 +33,33 @@ ClassImp(BasePlots)
   Double_t  BasePlots::eta_xmax  = 3.;
   Double_t* BasePlots::eta_array;
 
-
-BasePlots::BasePlots(){
-  this->makeBins(nch_nbin,nch_xmin,nch_xmax,nch_array);
-  this->makeBins(pt_nbin,pt_xmin,pt_xmax,pt_array);
-  this->makeBins(pt2_nbin,pt2_xmin,pt2_xmax,pt2_array);
-  this->makeBins(mt_nbin,mt_xmin,mt_xmax,mt_array);
-  this->makeBins(mpt_nbin,mpt_xmin,mpt_xmax,mpt_array);
-  this->makeBins(eta_nbin,eta_xmin,eta_xmax,eta_array);
+BasePlots::BasePlots(){  
+  this->makeBins(nch_nbin,nch_xmin,nch_xmax,&nch_array);
+  this->makeBins(pt_nbin,pt_xmin,pt_xmax,&pt_array);
+  this->makeBins(pt2_nbin,pt2_xmin,pt2_xmax,&pt2_array);
+  this->makeBins(mt_nbin,mt_xmin,mt_xmax,&mt_array);
+  this->makeBins(mpt_nbin,mpt_xmin,mpt_xmax,&mpt_array);
+  this->makeBins(eta_nbin,eta_xmin,eta_xmax,&eta_array);
   plotsname = "none";
 }
 
 BasePlots::BasePlots(TString collname){
-  this->makeBins(nch_nbin,nch_xmin,nch_xmax,nch_array);
-  this->makeBins(pt_nbin,pt_xmin,pt_xmax,pt_array);
-  this->makeBins(pt2_nbin,pt2_xmin,pt2_xmax,pt2_array);
-  this->makeBins(mt_nbin,mt_xmin,mt_xmax,mt_array);
-  this->makeBins(mpt_nbin,mpt_xmin,mpt_xmax,mpt_array);
-  this->makeBins(eta_nbin,eta_xmin,eta_xmax,eta_array);
+  this->makeBins(nch_nbin,nch_xmin,nch_xmax,&nch_array);
+  this->makeBins(pt_nbin,pt_xmin,pt_xmax,&pt_array);
+  this->makeBins(pt2_nbin,pt2_xmin,pt2_xmax,&pt2_array);
+  this->makeBins(mt_nbin,mt_xmin,mt_xmax,&mt_array);
+  this->makeBins(mpt_nbin,mpt_xmin,mpt_xmax,&mpt_array);
+  this->makeBins(eta_nbin,eta_xmin,eta_xmax,&eta_array);
   plotsname = collname;
 }
 
 BasePlots::~BasePlots(){ }
 
-void BasePlots::makeBins(const Int_t nbin , Double_t xmin , Double_t xmax , Double_t* array){
-  array = new Double_t[nbin+1];
+void BasePlots::makeBins(const Int_t nbin , Double_t xmin , Double_t xmax , Double_t** array){
+  *array = new Double_t[nbin+1];
   Double_t step = fabs( xmax - xmin ) / double (nbin);
   for(int i=0;i<=nbin;++i)
-    array[i] = xmin + i * step;
+    (*array)[i] = xmin + i * step;
 }
 
 void BasePlots::setBinning(vector< vector<double> >& array){
--- ChPartAna/plugins/BasePlots.h
+++ ChPartAna/plugins/BasePlots.h
@@ -4,6 +4,8 @@
 #include <vector>
 #include "TObject.h"
 #include "TString.h"
+#include <iostream>
+using namespace std;
 
 class BasePlots : public TObject {
 
@@ -46,12 +48,12 @@ class BasePlots : public TObject {
      static Double_t  eta_xmax;
      static Double_t* eta_array;
     
-    void setBinning_nch(Int_t nbin , Double_t xmin , Double_t xmax){nch_nbin =nbin;nch_xmin=xmin;nch_xmax=xmax;makeBins(nbin , xmin , xmax , nch_array);};
-    void setBinning_pt( Int_t nbin , Double_t xmin , Double_t xmax){pt_nbin  =nbin;pt_xmin =xmin;pt_xmax =xmax;this->makeBins(nbin , xmin , xmax , pt_array);};
-    void setBinning_pt2( Int_t nbin , Double_t xmin , Double_t xmax){pt2_nbin  =nbin;pt2_xmin =xmin;pt2_xmax =xmax;this->makeBins(nbin , xmin , xmax , pt2_array);};
-    void setBinning_mt( Int_t nbin , Double_t xmin , Double_t xmax){mt_nbin  =nbin;mt_xmin =xmin;mt_xmax =xmax;this->makeBins(nbin , xmin , xmax , mt_array);};
-    void setBinning_mpt(Int_t nbin , Double_t xmin , Double_t xmax){mpt_nbin =nbin;mpt_xmin=xmin;mpt_xmax=xmax;this->makeBins(nbin , xmin , xmax , mpt_array);};
-    void setBinning_eta(Int_t nbin , Double_t xmin , Double_t xmax){eta_nbin =nbin;eta_xmin=xmin;eta_xmax=xmax;this->makeBins(nbin , xmin , xmax , eta_array);};
+    void setBinning_nch(Int_t nbin , Double_t xmin , Double_t xmax){nch_nbin =nbin;nch_xmin=xmin;nch_xmax=xmax;makeBins(nbin , xmin , xmax , &nch_array);};
+    void setBinning_pt( Int_t nbin , Double_t xmin , Double_t xmax){pt_nbin  =nbin;pt_xmin =xmin;pt_xmax =xmax;this->makeBins(nbin , xmin , xmax , &pt_array);};
+    void setBinning_pt2( Int_t nbin , Double_t xmin , Double_t xmax){pt2_nbin  =nbin;pt2_xmin =xmin;pt2_xmax =xmax;this->makeBins(nbin , xmin , xmax , &pt2_array);};
+    void setBinning_mt( Int_t nbin , Double_t xmin , Double_t xmax){mt_nbin  =nbin;mt_xmin =xmin;mt_xmax =xmax;this->makeBins(nbin , xmin , xmax , &mt_array);};
+    void setBinning_mpt(Int_t nbin , Double_t xmin , Double_t xmax){mpt_nbin =nbin;mpt_xmin=xmin;mpt_xmax=xmax;this->makeBins(nbin , xmin , xmax , &mpt_array);};
+    void setBinning_eta(Int_t nbin , Double_t xmin , Double_t xmax){eta_nbin =nbin;eta_xmin=xmin;eta_xmax=xmax;this->makeBins(nbin , xmin , xmax , &eta_array);};
     
     void setBinning_nch(Double_t* array){ nch_array = array ;};
     void setBinning_pt(Double_t* array) { pt_array  = array ;};
@@ -62,7 +64,7 @@ class BasePlots : public TObject {
     
     void setBinning(vector< vector<double> >&);
     
-    void makeBins(const Int_t , Double_t , Double_t , Double_t*);
+    void makeBins(const Int_t , Double_t , Double_t , Double_t**);
     
   private:
 
