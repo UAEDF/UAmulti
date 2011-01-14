1f109f9923453a8f3cca477ff563aeeb93004412
rougny <>
2011-01-14 15:09:40 +0000

    added FBCorrelwCorr (trReweight included), changed getnCorrel to template ones, passed pt & eta cuts to default ones

--- ChPartAna/macro/FBana.C
+++ ChPartAna/macro/FBana.C
@@ -31,10 +31,10 @@ using namespace std;
 #include "../plugins/MultiPlots.h"
 #include "../plugins/GenMultiPlots.h"
 #include "../plugins/MatrixPlots.h"
-#include "../plugins/FBCorrel.h"
+#include "../plugins/FBCorrelwCorr.h"
 
 bool isMC  = true;
-bool debug = false;
+bool debug = 0;
 
 
 #include "FBacc.C"
@@ -50,21 +50,30 @@ TString st(string input , int cut){
 }
 
 
-void FBana(int type = 31 , double E = 7.0 , int iTracking = 1 , int nevt_max = 100){
+void FBana(int type = 31 , double E = 7.0 , int iTracking = 1 , int nevt_max = 10){
 
-  TTProfile::switchTH1On = 0;
-  TTProfile::switchTH2On = 1;
+  TTProfile::switchTH1On      = 1;
+  TTProfile::switchTH2On      = 1;
+  TTProfile::debug            = 0;
+  FBCorrel::debug             = 0;
+  FBCorrelwCorr::xCorrOn      = 0;
+  FBCorrelwCorr::xyCorrOn     = 1;
+  FBCorrelwCorr::trReweightOn = 1;
   
   if(debug) cout << "Getting the acceptance map ... " << endl;
   getAccMap();
   
   if(debug) cout << "Initializing classes ... " << endl;
-  vector<FBCorrel*> fbc_NSD_evtSel_reco_mnMVSnP (accMap.size(),0);
+  vector<FBCorrelwCorr*> fbcwc_NSD_evtSel_reco  (accMap.size(),0);
   vector<FBCorrel*> fbc_NSD_evtSel_reco         (accMap.size(),0);
+  vector<FBCorrel*> fbc_NSD_evtSel_reco_mnMVSnP (accMap.size(),0);
   vector<FBCorrel*> fbc_NSD_evtSel_gen          (accMap.size(),0);
   vector<FBCorrel*> fbc_NSD_noEvtSel_gen        (accMap.size(),0);
   vector<FBCorrel*> fbc_NSD_noEvtSel_gen_mnMVSnP(accMap.size(),0);
   vector<FBCorrel*> fbc_noEvtSel_gen            (accMap.size(),0);
+  
+  vector<TH1F*> diffWeightM (accMap.size(),0);
+  vector<TH1F*> diffWeightP (accMap.size(),0);
   
   
   for(int acc = 0 ; acc < (signed) accMap.size() ; ++acc){
@@ -75,20 +84,39 @@ void FBana(int type = 31 , double E = 7.0 , int iTracking = 1 , int nevt_max = 1
     //baseplot->setBinning(binning);
 
     //----------------------  RECO  ----------------------
-    fbc_NSD_evtSel_reco_mnMVSnP.at(acc)  = new FBCorrel(st("NSD_evtSel_reco_mnMVSnP",acc));
+    fbcwc_NSD_evtSel_reco.at(acc)        = new FBCorrelwCorr(st("NSD_evtSel_reco",acc));
     fbc_NSD_evtSel_reco.at(acc)          = new FBCorrel(st("NSD_evtSel_reco",acc));
+    fbc_NSD_evtSel_reco_mnMVSnP.at(acc)  = new FBCorrel(st("NSD_evtSel_reco_mnMVSnP",acc));
     fbc_NSD_evtSel_gen.at(acc)           = new FBCorrel(st("NSD_evtSel_gen",acc));
     fbc_NSD_noEvtSel_gen.at(acc)         = new FBCorrel(st("NSD_noEvtSel_gen",acc));
     fbc_NSD_noEvtSel_gen_mnMVSnP.at(acc) = new FBCorrel(st("NSD_noEvtSel_gen_mnMVSnP",acc));
-    fbc_noEvtSel_gen.at(acc)             = new FBCorrel(st("NSD_noEvtSel_gen",acc));
+    fbc_noEvtSel_gen.at(acc)             = new FBCorrel(st("noEvtSel_gen",acc));
+    
+    diffWeightM.at(acc) = new TH1F(st("diffWeightM",acc) , st("diffWeightM",acc) + ";(nTrReweight - nTr) / nTr; nEvts" , 600 , -3 , 3 );
+    diffWeightP.at(acc) = new TH1F(st("diffWeightP",acc) , st("diffWeightP",acc) + ";(nTrReweight - nTr) / nTr; nEvts" , 600 , -3 , 3 );
   }
   if(debug) cout << "                          ... Done !!";
+  
+  
+  //Getting the trm for the corrections
+  for(int acc = 0 ; acc < (signed) accMap.size() ; ++acc){
+    fbcwc_NSD_evtSel_reco.at(acc)->GetTRMFromFile("FBtrm1_type31_7TeV.root" , st("NSD_evtSel_nM",acc) , st("NSD_evtSel_nP",acc) );
+  }
+  
+  //Getting the 2D Eff for the corrections
+  for(int acc = 0 ; acc < (signed) accMap.size() ; ++acc){
+    fbcwc_NSD_evtSel_reco.at(acc)->GetTrEffFromFile("NCHtest1_type31_7TeV_mbTr.root" , "eff_ptVSeta_NSD_evtSel_matched" , "eff_ptVSeta_NSD_evtSel_fakes" );
+  }
+  
+  
+  
   
   if(type==0) isMC = false;
   TString MCtype = "pythia";
   if(type == 20) MCtype = "phojet";
   
   //adding files to the tree chain
+  cout << "Reading files : " << fileManager(0,type,E) << endl;
   TChain* tree = new TChain("evt","");
   tree->Add(fileManager(0,type,E));
   
@@ -149,11 +177,11 @@ void FBana(int type = 31 , double E = 7.0 , int iTracking = 1 , int nevt_max = 1
     vector<MyGenPart> gpcoll;
     pair<double,double> nCorrelGp = make_pair(0,0);
     if(isMC){
-      gpcoll = getPrimaryGenPart(*genPart , 0.1 , 2.5 , 0);
+      gpcoll = getPrimaryGenPart(*genPart);
       
       //Filling Inelastic Gen Level
       for(int acc = 0 ; acc < (signed) accMap.size() ; ++acc){
-        nCorrelGp = getnCorrelGenPart(gpcoll , accMap.at(acc));
+        nCorrelGp = getnCorrel(gpcoll , accMap.at(acc));
         fbc_noEvtSel_gen.at(acc)->Fill(nCorrelGp.first , nCorrelGp.second);
       }
       
@@ -162,7 +190,7 @@ void FBana(int type = 31 , double E = 7.0 , int iTracking = 1 , int nevt_max = 1
       
       //Filling NSD Gen Level
       for(int acc = 0 ; acc < (signed) accMap.size() ; ++acc){
-        nCorrelGp = getnCorrelGenPart(gpcoll , accMap.at(acc));
+        nCorrelGp = getnCorrel(gpcoll , accMap.at(acc));
         fbc_NSD_noEvtSel_gen.at(acc)->Fill(nCorrelGp.first , nCorrelGp.second);
         fbc_NSD_noEvtSel_gen_mnMVSnP.at(acc)->Fill(nCorrelGp.second , nCorrelGp.first);
       }
@@ -175,21 +203,31 @@ void FBana(int type = 31 , double E = 7.0 , int iTracking = 1 , int nevt_max = 1
     //Filling NSD Gen Level AFTER evtSel
     if(isMC){
       for(int acc = 0 ; acc < (signed) accMap.size() ; ++acc){
-        nCorrelGp = getnCorrelGenPart(gpcoll , accMap.at(acc));
+        nCorrelGp = getnCorrel(gpcoll , accMap.at(acc));
         fbc_NSD_evtSel_gen.at(acc)->Fill(nCorrelGp.first , nCorrelGp.second);
       }
     }
     
     
     vector<MyTracks> trcoll;
-    if(iTracking==0) trcoll = getPrimaryTracks(*tracks,vertex,0.1,2.5,0);
-    if(iTracking==1) trcoll = getPrimaryTracks(*tracks,vertex,bs,0.1,2.5,0);
-    
-    pair<double,double> nCorrelTr = make_pair(0,0);
+    if(iTracking==0) trcoll = getPrimaryTracks(*tracks,vertex);
+    if(iTracking==1) trcoll = getPrimaryTracks(*tracks,vertex,bs);
+   
+    pair<double,double> nCorrelTr         = make_pair(0,0);
+    pair<double,double> nCorrelTrReweight = make_pair(0,0);
     for(int acc = 0 ; acc < (signed) accMap.size() ; ++acc){
-      nCorrelTr = getnCorrelTracks(trcoll , accMap.at(acc));
+      nCorrelTr = getnCorrel(trcoll , accMap.at(acc));
+      fbcwc_NSD_evtSel_reco.at(acc)->Fill(nCorrelTr.first , nCorrelTr.second);
       fbc_NSD_evtSel_reco.at(acc)->Fill(nCorrelTr.first , nCorrelTr.second);
       fbc_NSD_evtSel_reco_mnMVSnP.at(acc)->Fill(nCorrelTr.second , nCorrelTr.first);
+      
+      nCorrelTrReweight = getnCorrelTrReweight(trcoll , accMap.at(acc) , fbcwc_NSD_evtSel_reco.at(acc)->trEff , fbcwc_NSD_evtSel_reco.at(acc)->trFakes);
+      fbcwc_NSD_evtSel_reco.at(acc)->FillTrReweight(nCorrelTrReweight.first , nCorrelTrReweight.second);
+      
+      if(nCorrelTr.first>0)
+        diffWeightM.at(acc)->Fill( (nCorrelTrReweight.first - nCorrelTr.first) / nCorrelTr.first);
+      if(nCorrelTr.second>0)
+        diffWeightP.at(acc)->Fill( (nCorrelTrReweight.second - nCorrelTr.second) / nCorrelTr.second);
     }
     
   }//end of loop over events
@@ -197,7 +235,8 @@ void FBana(int type = 31 , double E = 7.0 , int iTracking = 1 , int nevt_max = 1
   
   //output file
   ostringstream strout("FBtest");
-  strout<<"FBtest_type"<<type<<"_"<<E<<"TeV.root";
+  strout<<"FBtest9_type"<<type<<"_"<<E<<"TeV.root";
+  cout << "Output File : " << strout.str() << endl; 
   TFile* output = new TFile(strout.str().c_str(),"RECREATE");
   output->cd();
   
@@ -205,6 +244,7 @@ void FBana(int type = 31 , double E = 7.0 , int iTracking = 1 , int nevt_max = 1
     //if(isMC) fbc_NSD_noEvtSel_gen.at(acc)->ttp.write(1,1);
     //fbc_NSD_evtSel_reco.at(acc)->ttp.write(1,1);
     
+    fbcwc_NSD_evtSel_reco.at(acc)->write();
     fbc_NSD_evtSel_reco.at(acc)->write();
     fbc_NSD_evtSel_reco_mnMVSnP.at(acc)->write();
     if(isMC){
@@ -212,6 +252,10 @@ void FBana(int type = 31 , double E = 7.0 , int iTracking = 1 , int nevt_max = 1
       fbc_NSD_noEvtSel_gen.at(acc)->write();
       fbc_NSD_noEvtSel_gen_mnMVSnP.at(acc)->write();
     }
+    
+    
+    diffWeightM.at(acc)->Write();
+    diffWeightP.at(acc)->Write();
   }
   output->Close();
 }
