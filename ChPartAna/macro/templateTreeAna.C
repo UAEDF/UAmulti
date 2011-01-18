cddc8bce86a76b978a14d3b8fb596a6ce3ecd590
rougny <>
2011-01-18 12:06:57 +0000

    passed to new way of opening files

--- ChPartAna/macro/templateTreeAna.C
+++ ChPartAna/macro/templateTreeAna.C
@@ -8,6 +8,7 @@
 #include <TTree.h>
 #include <TFile.h>
 #include <TChain.h>
+#include <TChainElement.h>
 #include <TDirectory.h>
 
 //STANDARD C++ INCLUDES
@@ -31,7 +32,7 @@ bool debug = false;
 
 //USED TO KNOW WHICH FILE TO GET
 //NOT USEFUL IF NOT RUNNING ON THE T2
-//#include "fileManager.C"
+#include "fileManager.C"
 
 //USED TO SELECT GOOD TRACKS OR GenPart
 #include "cuts.C"
@@ -56,98 +57,107 @@ void templateTreeAna(int type = 0 , double E = 0.9 , int iTracking = 1 , int nev
   TH1F* pt_gen  = new TH1F("pt_gen" , "pt_gen;pt;nTracks" , 120 , 0 , 6);
   TH1F* pt_reco = new TH1F("pt_reco", "pt_reco;pt;nTracks" , 120 , 0 , 6);
   
-  //adding files to the tree chain
-  TChain* tree = new TChain("evt","");
-  tree->Add("../data_7TeV.root");
+  //getting the list of files
+  vector<TString>* vfiles = getListOfFiles(fileManager(0,type,E));
   
-  
-  //adding branches to the tree ----------------------------------------------------------------------
-  
-  MyEvtId* evtId      = NULL ;
-  tree->SetBranchAddress("EvtId",&evtId);
-  
-  vector<MyGenPart>* genPart = NULL;
-  if(isMC) tree->SetBranchAddress("GenPart",&genPart);
-  
-  MyGenKin* genKin = NULL;
-  if(isMC) tree->SetBranchAddress("GenKin",&genKin);
-  
-  vector<MyTracks>*  tracks  = NULL;
-  vector<MyVertex>*  vertex  = NULL;
+  //Declaration of tree and its branches variables
+  TTree* tree = new TChain("evt","");
+  MyEvtId*           evtId        = NULL ;
+  vector<MyGenPart>* genPart      = NULL;
+  MyGenKin*          genKin       = NULL;
+  vector<MyTracks>*  tracks       = NULL;
+  vector<MyVertex>*  vertex       = NULL;
   vector<MyVertex>*  vertexToCut  = NULL;
-  MyL1Trig*     L1Trig    = NULL;
-  MyMITEvtSel*  MITEvtSel = NULL;
-  MyBeamSpot*   bs        = NULL;
-  if(iTracking==0){
-    tree->SetBranchAddress("generalTracks",&tracks); 
-    tree->SetBranchAddress("primaryVertex",&vertex);
-  }
-  else if(iTracking==1){
-    tree->SetBranchAddress("minbiasTracks",&tracks); 
-    tree->SetBranchAddress("ferencVtxFerTrk",&vertex);
-  }
+  MyL1Trig*          L1Trig       = NULL;
+  MyMITEvtSel*       MITEvtSel    = NULL;
+  MyBeamSpot*        bs           = NULL;
   
-  tree->SetBranchAddress("pixel3Vertex",&vertexToCut);
-  tree->SetBranchAddress("L1Trig",&L1Trig);
-  tree->SetBranchAddress("MITEvtSel",&MITEvtSel);
-  tree->SetBranchAddress("beamSpot",&bs);
+  int i_tot = 0 , nevt_tot = 0;
+  //starting Loop over files, stops at end of list of files or when reached nevt_max
+  for(vector<TString>::iterator itfiles = vfiles->begin() ; itfiles != vfiles->end() && i_tot < nevt_max ; ++itfiles){
   
-
-  //Getting number of events
-  int nev = int(tree->GetEntries());
-  std::cout <<"number of entries is : "<< nev << std::endl;
-  cout<<"Running on: "<<min(nev,nevt_max)<<" events"<<endl;
+    TFile* file = TFile::Open(*itfiles,"READ");
+    
+    //getting the tree form the current file
+    tree = (TTree*) file->Get("evt");
+    
+    //adding branches to the tree ----------------------------------------------------------------------
+    tree->SetBranchAddress("EvtId",&evtId);
+    if(isMC) tree->SetBranchAddress("GenPart",&genPart);
+    if(isMC) tree->SetBranchAddress("GenKin",&genKin);
+    if(iTracking==0){
+      tree->SetBranchAddress("generalTracks",&tracks); 
+      tree->SetBranchAddress("primaryVertex",&vertex);
+    }
+    else if(iTracking==1){
+      tree->SetBranchAddress("minbiasTracks",&tracks); 
+      tree->SetBranchAddress("ferencVtxFerTrk",&vertex);
+    }
+    tree->SetBranchAddress("pixel3Vertex",&vertexToCut);
+    tree->SetBranchAddress("L1Trig",&L1Trig);
+    tree->SetBranchAddress("MITEvtSel",&MITEvtSel);
+    tree->SetBranchAddress("beamSpot",&bs);
   
-  //starting loop over events ----------------------------------------------------------------------
-  for(int i = 0; i < nev; i++){
-    
-    //printing the % of events done every 10k evts
-    if( ((i+1) % 10000) == 0) cout <<int(double(i+1)/double(min(nev,nevt_max))*100.)<<" % done"<<endl;
-    
-    if(i>min(nev,nevt_max)) break;
-    
-    //Filling the variables defined setting branches
-    tree->GetEntry(i);
 
     //Just to put the good collection of vertex in vertexToCut
-     if(i==0) vertexToCut = vertex;
+    vertexToCut = vertex;
+
+    //Getting number of events
+    int nev = int(tree->GetEntriesFast());
+    nevt_tot += nev;
+    cout <<"The current file has " << nev << " entries : "<< endl << *itfiles << endl;
+    //cout<<"Running on: "<<min(nev,nevt_max)<<" events"<<endl;
+  
+    //starting loop over events, stops when reached end of file or nevt_max
+    for(int i = 0; i < nev && i_tot < nevt_max; ++i , ++i_tot){
+    
+      //printing the % of events done every 10k evts
+      if( ((i_tot+1) % 10000) == 0) cout <<int(double(i_tot+1)/1000)<<"k done"<<endl;
+    
+      //if(i>min(nev,nevt_max)) break;
+    
+      //Filling the variables defined setting branches
+      tree->GetEntry(i);
 
 
-    //AT THIS POINT ON, CAN START USING THE VARIABLES LIKE TRACKS, VERTEX ETC !
+      //AT THIS POINT ON, CAN START USING THE VARIABLES LIKE TRACKS, VERTEX ETC !
 
 
-    //-------------------------------------------------------------------------------------------------
-    //filling pt distribution for the generated particles
-    //ie those from pythia generator, without reconstruction
-    if(isMC){
-      for(vector<MyGenPart>::iterator p=genPart->begin() ; p!=genPart->end() ; p++ )
+      //-------------------------------------------------------------------------------------------------
+      //filling pt distribution for the generated particles
+      //ie those from pythia generator, without reconstruction
+      if(isMC){
+        for(vector<MyGenPart>::iterator p=genPart->begin() ; p!=genPart->end() ; p++ )
           pt_gen->Fill(p->Part.v.Pt());
-    }
+      }
 
 
-
-    //Skipping the Single-Diffractive events starting from here
-    //function isSD() defined in evtSel.C
-    if(isMC)
-      if(isSD(genKin , MCtype) ) continue;
+      //Skipping the Single-Diffractive events starting from here
+      //function isSD() defined in evtSel.C
+      if(isMC)
+        if(isSD(genKin , MCtype) ) continue;
       
 
-    //-------------------------------------------------------------------------------------------------
-    //skipping events that don't pass our event selection
-    //function isEvtGood() defined in evtSel.C
-    if(!isEvtGood(E,*L1Trig , *MITEvtSel , vertexToCut)) continue;
+      //cout << "debug5" << endl;
+      //-------------------------------------------------------------------------------------------------
+      //skipping events that don't pass our event selection
+      //function isEvtGood() defined in evtSel.C
+      if(!isEvtGood(E,*L1Trig , *MITEvtSel , vertexToCut)) continue;
     
+
+      //-------------------------------------------------------------------------------------------------
+      //filling pt distribution for the observed/reconstructed tracks in the detector
+      for(vector<MyTracks>::iterator it_tr = tracks->begin() ; it_tr != tracks->end() ; ++it_tr)
+        pt_reco->Fill(it_tr->Part.v.Pt());
+
+
+
+    }//end of loop over events
     
-
-    //-------------------------------------------------------------------------------------------------
-    //filling pt distribution for the observed/reconstructed tracks in the detector
-    for(vector<MyTracks>::iterator it_tr = tracks->begin() ; it_tr != tracks->end() ; ++it_tr)
-      pt_reco->Fill(it_tr->Part.v.Pt());
-
-
-
-
-  }//end of loop over events
+    //Closing current files
+    file->Close();
+    
+  }//end of loop over files
   
   
   //output file
