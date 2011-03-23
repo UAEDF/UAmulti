0ac15c79c9f09ae94a89bf328446f63e3b3e7d9d
sluyckx <>
2011-03-23 15:51:41 +0000

    added sys100 and sys302 to the bash script

--- ChPartAna/macro/unfold.sh
+++ ChPartAna/macro/unfold.sh
@@ -1,39 +1,92 @@
-  #Bash Unfolding
-unf ()
+#Bash Unfolding
+#   nohup bash unfold.sh 0 0 true 0 > logUnf0_0_true.txt &
+#   nohup bash unfold.sh 0 0 true 1 > logUnf0_0_true_AllSys.txt &
+
+unfall() 
+{
+    unf  $1 $2 $3 $4 true
+    unf  $1 $2 $3 $4 false
+}
+
+unf()
 {
     echo "unfolding cut $1 till $2"
+    stringDir=v11
     for ((i = $1; i<=$2; i+=1));
     do 
-        echo "unfolding cut $i "
-       # root -l -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"v5\", 60, -1, -1, $3, $i )" -q
-      # root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"v5\", 60, 500000, 500000, $3, $i )" -q
-      echo'-----'
+       echo "unfolding cut $i "
+       echo '----'
        echo "MC10"
        echo '----'      
-       root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"v5b\", 10, 0, 2000000, 342220, $3, $i )" -q    
-       root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"v5b\", 10, 1, 2000000, 342220, $3, $i )" -q
-       echo'-----'
-       echo "MC15"
+       if [ $4 = 0 ]; then      
+                                                                            #string         MC  HF   nMC      ndata                   nSys +/-
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 10, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 0, 0 , $5 )" -q       
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 10, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 0, 0 , $5 )" -q 
+       else           
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 10, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 100,  1 , $5 )" -q       
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 10, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 100, -1 , $5 )" -q        
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 10, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 100,  1 , $5 )" -q         
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 10, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 100, -1 , $5 )" -q          
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 10, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 302,  1 , $5 )" -q       
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 10, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 302, -1 , $5 )" -q        
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 10, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 302,  1 , $5 )" -q         
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 10, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 302, -1 , $5 )" -q     
+       fi
        echo '----'
-       root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"v5b\", 15, 0, 2000000, 342220, $3, $i )" -q
-       root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"v5b\", 15, 1, 2000000, 342220, $3, $i )" -q
-       echo'-----'
+       echo "MC15"
+       echo '----'       
+       if [ $4 = 0 ]; then      
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 15, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 0, 0 , $5 )" -q       
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 15, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 0, 0 , $5 )" -q 
+       else           
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 15, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 100,  1 , $5 )" -q       
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 15, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 100, -1 , $5 )" -q        
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 15, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 100,  1 , $5 )" -q         
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 15, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 100, -1 , $5 )" -q          
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 15, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 302,  1 , $5 )" -q       
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 15, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 302, -1 , $5 )" -q        
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 15, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 302,  1 , $5 )" -q         
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 15, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 302, -1 , $5 )" -q      
+       fi
+       echo '----'
        echo "MC31"
        echo '----'       
-       root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"v5b\", 31, 0, 1886500, 342220, $3, $i )" -q       
-       root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"v5b\", 31, 1, 1886500, 342220, $3, $i )" -q
-       echo'-----'
+       if [ $4 = 0 ]; then      
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 31, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 0, 0 , $5 )" -q       
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 31, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 0, 0 , $5 )" -q 
+       else           
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 31, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 100,  1 , $5 )" -q       
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 31, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 100, -1 , $5 )" -q        
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 31, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 100,  1 , $5 )" -q         
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 31, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 100, -1 , $5 )" -q          
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 31, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 302,  1 , $5 )" -q       
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 31, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 302, -1 , $5 )" -q        
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 31, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 302,  1 , $5 )" -q         
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 31, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 302, -1 , $5 )" -q  
+       fi
+       echo '----'                                                    
        echo "MC60"
-       echo '----'
-       root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"v5b\", 60, 0, 2000000, 342220, $3, $i )" -q
-       root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"v5b\", 60, 1, 2000000, 342220, $3, $i )" -q
-       # root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"v5\", 15, -1, -1, false, $i )" -q
+       echo '----'       
+       if [ $4 = 0 ]; then
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 60, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 0, 0 , $5 )" -q
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 60, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 0, 0 , $5 )" -q      
+       else
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 60, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 100,  1 , $5 )" -q       
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 60, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 100, -1 , $5 )" -q        
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 60, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 100,  1 , $5 )" -q         
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 60, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 100, -1 , $5 )" -q          
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 60, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 302,  1 , $5 )" -q       
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 60, 0, 5000000, 342220, $3, $i, 7.0, 7.0, 302, -1 , $5 )" -q        
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 60, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 302,  1 , $5 )" -q         
+          root -l ../macro/BuildLibDico.C+ ../scripts/NCHmakeCorrections.C+"(\"$stringDir\", 60, 1, 5000000, 342220, $3, $i, 7.0, 7.0, 302, -1 , $5 )" -q  
+       fi
     done 
 }
 
 #Run different line for each m machine
 #unf cut_start cut_end  true/false   (true or false for useData or MCcorrMC...)
-unf $1 $2 $3
+unfall $1 $2 $3 $4 
 
 echo "job finished"   
-#   nohup bash unfold.sh 0 0 true > logUnf0_0_true.txt &
+#   nohup bash unfold.sh 0 0 true 0 > logUnf0_0_true.txt &
+#   nohup bash unfold.sh 0 5 true 1 > logUnf0_5_true_AllSys.txt &
