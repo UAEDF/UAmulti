7351f423d1d52f7adf36b45fe0fc515d27bb2fa9
sluyckx <>
2011-04-01 18:31:36 +0000

    first commit

---
+++ ChPartAna/macro/NCHbinningMap.C
@@ -0,0 +1,218 @@
+vector<double> makeBins(const Int_t nbin , Double_t xmin , Double_t xmax){
+  vector<double> array(nbin+1,0);
+  Double_t step = fabs( xmax - xmin ) / double (nbin);
+  for(int i=0;i<=nbin;++i)
+    array[i] = xmin + i * step;
+    return array;
+}
+
+void changeBins(int ibinmin , int ibinmax , double step , vector<double>& array){
+  for(int i=0;i<=(ibinmax-ibinmin);++i)
+    array[ibinmin+i] = array[ibinmin] + i * step;
+ // return array;
+}
+
+void addBins(const int nbinnew , double step , vector<double>& array){
+  int nbinold = array.size()-1;
+  for(int i=1;i<=nbinnew;++i)
+    array.push_back( array[nbinold] + i * step );
+ // return array;
+}
+
+vector< vector<double> > getBins(int inch , int ipt , int ieta){
+  vector< vector<double> > bins;
+  
+  //---------------- NCH BINNING ---------------
+  if(inch==0){
+    bins.push_back(makeBins(171,-0.5,170.5));
+  }
+  else if(inch==1){
+    vector<double> tmp = makeBins(63,-0.5,62.5);
+    addBins(3,2,tmp);
+    addBins(10,5,tmp);
+    bins.push_back(tmp);
+  }
+  
+  //---------------- PT BINNING ---------------
+  if(ipt==0){
+    bins.push_back(makeBins(30,0.,3.));
+  }
+  
+  //---------------- ETA BINNING ---------------
+  if(ieta==0){
+    bins.push_back(makeBins(60,-3.,3.));
+  }
+  else if(ieta==1){
+    bins.push_back(makeBins(20,-2.4,2.4));
+  }
+  else if(ieta==2){
+    bins.push_back(makeBins(12,-3.,3.));
+  }
+  
+  return bins;
+}
+
+
+//used with the output of getLowStatBinsv2() !!
+vector< vector<double> > getBins(int icut , double Ebinning = 0.9){
+  vector< vector<double> > bins;
+  vector<double> tmp = makeBins(371,-0.5,370.5);
+  
+//--------------------------------------------------
+//-------------     All bins 1     -----------------
+//--------------------------------------------------
+if(Ebinning==1)
+  tmp = makeBins(371,-0.5,370.5);
+
+
+
+
+  //!!!!!!!!!!!!! HERE USED MBTRACKS FOR BINNING
+  
+//--------------------------------------------------
+//-------------      0.9 TeV      ------------------
+//--------------------------------------------------
+
+if(Ebinning==0.9)
+{
+  if(icut==0){
+    tmp = makeBins(74,-0.5,73.5);
+    addBins(1,2,tmp);
+    addBins(1,3,tmp);
+    addBins(1,4,tmp);
+    addBins(1,10,tmp);
+    addBins(21,10,tmp);
+  }
+  if(icut==1){
+    tmp = makeBins(74,-0.5,73.5);
+    addBins(1,2,tmp);
+    addBins(1,3,tmp);
+    addBins(1,4,tmp);
+    addBins(1,10,tmp);
+    addBins(21,10,tmp);
+  }
+  if(icut==2){
+    tmp = makeBins(39,-0.5,38.5);
+    addBins(1,2,tmp);
+    addBins(1,2,tmp);
+    addBins(1,10,tmp);
+    addBins(25,10,tmp);
+  }
+  if(icut==3){
+    tmp = makeBins(19,-0.5,18.5);
+    addBins(1,2,tmp);
+    addBins(56,5,tmp);
+  }
+  if(icut==4){
+    tmp = makeBins(41,-0.5,40.5);
+    addBins(1,2,tmp);
+    addBins(1,3,tmp);
+    addBins(51,5,tmp);
+  }
+  if(icut==5){
+    tmp = makeBins(41,-0.5,40.5);
+    addBins(1,2,tmp);
+    addBins(1,3,tmp);
+    addBins(51,5,tmp);
+  }
+
+}
+  
+   
+  
+//--------------------------------------------------
+//-------------      7.0  TeV      -----------------
+//--------------------------------------------------
+if(Ebinning==7.0 ){
+
+  if(icut==0){
+    tmp = makeBins(98,-0.5,97.5);
+    addBins(1,2,tmp);
+    addBins(1,2,tmp);
+    addBins(1,2,tmp);
+    addBins(1,2,tmp);
+    addBins(1,2,tmp);
+    addBins(1,2,tmp);
+    addBins(1,3,tmp);
+    addBins(1,4,tmp);
+    addBins(1,5,tmp);
+    addBins(1,9,tmp);
+    addBins(19,9,tmp);
+  }
+  if(icut==1){
+    tmp = makeBins(98,-0.5,97.5);
+    addBins(1,2,tmp);
+    addBins(1,2,tmp);
+    addBins(1,2,tmp);
+    addBins(1,2,tmp);
+    addBins(1,2,tmp);
+    addBins(1,2,tmp);
+    addBins(1,3,tmp);
+    addBins(1,4,tmp);
+    addBins(1,5,tmp);
+    addBins(1,9,tmp);
+    addBins(19,9,tmp);
+  }
+  if(icut==2){
+    tmp = makeBins(57,-0.5,56.5);
+    addBins(1,2,tmp);
+    addBins(1,2,tmp);
+    addBins(1,2,tmp);
+    addBins(1,3,tmp);
+    addBins(1,6,tmp);
+    addBins(38,6,tmp);
+  }
+  if(icut==3){
+    tmp = makeBins(29,-0.5,28.5);
+    addBins(1,2,tmp);
+    addBins(1,2,tmp);
+    addBins(1,4,tmp);
+    addBins(53,5,tmp);
+  }
+  if(icut==4){
+    tmp = makeBins(52,-0.5,51.5);
+    addBins(1,2,tmp);
+    addBins(1,2,tmp);
+    addBins(1,2,tmp);
+    addBins(1,3,tmp);
+    addBins(1,8,tmp);
+    addBins(29,8,tmp);
+  }
+  if(icut==5){
+    tmp = makeBins(52,-0.5,51.5);
+    addBins(1,2,tmp);
+    addBins(1,2,tmp);
+    addBins(1,2,tmp);
+    addBins(1,3,tmp);
+    addBins(1,8,tmp);
+    addBins(29,8,tmp);
+  }
+
+} 
+
+  bins.push_back(tmp);
+  
+  //adding default pt 
+  // bins.push_back(makeBins(30,0.,3.));
+  tmp.clear();
+  tmp = makeBins(200,0.,4.);
+//  addBins(80,.1,tmp);
+  bins.push_back(tmp);
+
+  // eta bins
+  bins.push_back(makeBins(24,-2.4,2.4));
+
+  // pt
+  tmp.clear();
+  tmp = makeBins(200,0.,4.);
+  addBins(80,.1,tmp);
+  bins.push_back(tmp);
+
+  // mt
+  tmp.clear();
+  tmp = makeBins(200,0.,2.);
+  addBins(80,.1,tmp);
+  bins.push_back(tmp);
+
+  return bins;
+}
---
+++ ChPartAna/scripts/NCHgetGoodBinning.C
@@ -0,0 +1,126 @@
+#include "TMath.h"
+#include "TH1F.h"
+#include "TF1.h"
+#include "TH1D.h"
+#include "TH2F.h"
+#include "TFile.h"
+#include "TCanvas.h"
+#include "TStyle.h"
+#include "TLegend.h"
+#include "TPad.h"
+#include "TRandom.h"
+#include "TGraphAsymmErrors.h"
+#include "TProfile.h"
+
+#include "TFrame.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+//_____________________________________________________________________________
+TString st(string input , int cut){
+  stringstream nb("");  
+  nb<<cut;
+  return input+"cut"+nb.str();
+}
+
+// root -l NCHgetGoodBinning.C+"(\"../macro/outputs_full/v21/output_data_ferncTr_E_7_3422minEvtInBin.root\")" -q
+// root -l NCHgetGoodBinning.C+"(\"../macro/outputs_full/v21/output_data_ferncTr_E_7_342220.root\")" -q
+void NCHgetGoodBinning(TString filename){
+  double minEvtInBin = 40;  
+  #include "../macro/acceptanceMap.C"
+  
+  cout<<"File : "<<filename<<endl;
+  TFile* file = new TFile(filename,"READ");
+  
+
+  //Getting the good dir
+  for(int acc = 0 ; acc < (signed)accMap.size() ; ++acc){
+
+    TString histname = "nch_full_HF0_nocut_RECO";
+
+    cout<<"  if(icut=="<<acc<<"){"<<endl;
+    
+    //cout<<histname.str()<<endl;
+    TH1F* hist = NULL;
+    hist = (TH1F*) file->FindObjectAny(histname+st("_",acc));
+    if (hist==0) {
+        cout << " histogram does not exist for: acc " << acc << endl;
+        return;
+    }    
+    
+    int lastwidth = 1;
+    bool first = false;
+    for(int i=1 ; i<=hist->GetNbinsX() ; ++i){
+      int binwidth = int(hist->GetBinWidth(i));
+      
+      //making first bins with width 1
+      if(hist->GetBinContent(i)<minEvtInBin && hist->GetBinContent(i)!=0 && !first && binwidth==1){
+        cout<<"    tmp = makeBins("<<i-1<<",-0.5,"<<double(i)-1.5<<");"<<endl;
+        first=true;
+      }
+      
+      //making first bins with width=1,then others with width!=1 && stat>minEvtInBin
+      if(hist->GetBinContent(i)>=minEvtInBin && binwidth!=1 && !first){
+        cout<<"    tmp = makeBins("<<i-1<<",-0.5,"<<double(i)-1.5<<");"<<endl;
+	first = true;
+      }
+      
+      if(hist->GetBinContent(i)>=minEvtInBin && first){
+        while(binwidth<lastwidth){
+	  i++;
+	  binwidth+=int(hist->GetBinWidth(i));
+	}
+        cout<<"    addBins(1,"<<binwidth<<",tmp);"<<endl;
+      }
+      //making bins with stat<minEvtInBin
+      if(hist->GetBinContent(i)<minEvtInBin){
+        first = true;
+        double n = hist->GetBinContent(i);
+	int j = 0;
+	bool useLastBin = false;
+	int nnull = 0;
+        for(j=i+1;j<=hist->GetNbinsX() ; ++j){
+	  if(hist->GetBinContent(j)==0) ++nnull;
+          n+=hist->GetBinContent(j);
+	  binwidth += int(hist->GetBinWidth(j));
+	  
+	  if(nnull>5){useLastBin=true;break;}
+	  if(n>minEvtInBin) break;
+        }
+        
+	if(!useLastBin && binwidth<=10){
+	  while(binwidth<lastwidth){
+	    j++;
+	    binwidth+=int(hist->GetBinWidth(j));
+	  }
+          cout<<"    addBins(1,"<<binwidth<<",tmp);"<<endl;
+	  i=j;
+	}
+	
+	if(!useLastBin && binwidth>10){
+          cout<<"    addBins(1,10,tmp);"<<endl;
+	  binwidth=10;
+	  i+=10;
+	}
+	if(useLastBin){
+	  if(lastwidth<=5)
+	    binwidth=5;
+	  else
+	    binwidth=lastwidth;
+	    
+	  int nbin = int((300-i)/binwidth);
+	  cout<<"    addBins("<<nbin+1<<","<<binwidth<<",tmp);"<<endl;
+	  break;
+	}
+	
+      }
+      lastwidth = binwidth;
+    }
+    cout<<"  }"<<endl;
+  }//end of loop over acceptance map
+}
+
+
+
