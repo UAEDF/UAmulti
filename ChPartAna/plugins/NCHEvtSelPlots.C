99997699c3ccede0ed15801673d0649a6231c55b
sluyckx <>
2011-04-14 07:06:39 +0000

    changed double naming problem

--- ChPartAna/plugins/NCHEvtSelPlots.C
+++ ChPartAna/plugins/NCHEvtSelPlots.C
@@ -76,53 +76,53 @@ void NCHEvtSelPlots::init(){
   
     //Particles 
     if(switchIntermedPlots){        
-        gpp_b40Sel                    = new GenPartPlots("b40Sel_"+trackcoll);
-        gpp_L1Sel                     = new GenPartPlots("L1Sel_"+trackcoll);
-        gpp_hfSel                     = new GenPartPlots("hfSel_"+trackcoll);
-        gpp_vtxqualSel                = new GenPartPlots("vtxqualSel_"+trackcoll);
-        gpp_vtxSel                    = new GenPartPlots("vtxSel_"+trackcoll);
-        gpp_L1_hfSel                  = new GenPartPlots("L1_hfSel_"+trackcoll);
-        gpp_L1_hf_vtxqualSel          = new GenPartPlots("L1_hf_vtxqualSel_"+trackcoll);
+        gpp_b40Sel                    = new GenPartPlots("gpp_b40Sel_"+trackcoll);
+        gpp_L1Sel                     = new GenPartPlots("gpp_L1Sel_"+trackcoll);
+        gpp_hfSel                     = new GenPartPlots("gpp_hfSel_"+trackcoll);
+        gpp_vtxqualSel                = new GenPartPlots("gpp_vtxqualSel_"+trackcoll);
+        gpp_vtxSel                    = new GenPartPlots("gpp_vtxSel_"+trackcoll);
+        gpp_L1_hfSel                  = new GenPartPlots("gpp_L1_hfSel_"+trackcoll);
+        gpp_L1_hf_vtxqualSel          = new GenPartPlots("gpp_L1_hf_vtxqualSel_"+trackcoll);
     }
     //needed for efficiency plots
-    gpp_noSel                         = new GenPartPlots("noSel_"+trackcoll);    
+    gpp_noSel                         = new GenPartPlots("gpp_noSel_"+trackcoll);    
     //gpp_full                          = new GenPartPlots("full_"+trackcoll);
-    mppart_noSel                      = new MultiPlots("partnoSel_"+trackcoll);
-    mppart_full                       = new MultiPlots("partfull_"+trackcoll);    
+    mppart_noSel                      = new MultiPlots("mp_partnoSel_"+trackcoll);
+    mppart_full                       = new MultiPlots("mp_partfull_"+trackcoll);    
     //matrix
     mtx_full                          = new TResponseMtx("partfull_"+trackcoll, 
                                 vector<Double_t>( mppart_full->nch_array, mppart_full->nch_array+mppart_full->nch_nbin+1) );  
                                 
-    gpp_CentralGen                    = new GenPartPlots("CentrGen_"+trackcoll);         
+    gpp_CentralGen                    = new GenPartPlots("gpp_CentrGen_"+trackcoll);         
   }
   
   //Reconstructed Tracks
   if(switchIntermedPlots){
-    trp_b40Sel                        = new TrackPlots("b40Sel_"+trackcoll);
-    trp_L1Sel                         = new TrackPlots("L1Sel_"+trackcoll);
-    trp_hfSel                         = new TrackPlots("hfSel_"+trackcoll);
-    trp_vtxqualSel                    = new TrackPlots("vtxqualSel_"+trackcoll);
-    trp_vtxSel                        = new TrackPlots("vtxSel_"+trackcoll);
-    trp_L1_hfSel                      = new TrackPlots("L1_hfSel_"+trackcoll);
-    trp_L1_hf_vtxqualSel              = new TrackPlots("L1_hf_vtxqualSel_"+trackcoll);
+    trp_b40Sel                        = new TrackPlots("trp_b40Sel_"+trackcoll);
+    trp_L1Sel                         = new TrackPlots("trp_L1Sel_"+trackcoll);
+    trp_hfSel                         = new TrackPlots("trp_hfSel_"+trackcoll);
+    trp_vtxqualSel                    = new TrackPlots("trp_vtxqualSel_"+trackcoll);
+    trp_vtxSel                        = new TrackPlots("trp_vtxSel_"+trackcoll);
+    trp_L1_hfSel                      = new TrackPlots("trp_L1_hfSel_"+trackcoll);
+    trp_L1_hf_vtxqualSel              = new TrackPlots("trp_L1_hf_vtxqualSel_"+trackcoll);
   } 
-  trp_noSel                           = new TrackPlots("noSel_"+trackcoll);  
-  trp_full                            = new TrackPlots("full_"+trackcoll);
+  trp_noSel                           = new TrackPlots("trp_noSel_"+trackcoll);  
+  trp_full                            = new TrackPlots("trp_full_"+trackcoll);
   
   if(switchIntermedPlots){ 
-    vtxp_b40Sel                       = new VertexPlots("b40Sel_"+trackcoll);
-    vtxp_L1Sel                        = new VertexPlots("L1Sel_"+trackcoll);
-    vtxp_hfSel                        = new VertexPlots("hfSel_"+trackcoll);
-    vtxp_vtxqualSel                   = new VertexPlots("vtxqualSel_"+trackcoll);
-    vtxp_vtxSel                       = new VertexPlots("vtxSel_"+trackcoll);
-    vtxp_L1_hfSel                     = new VertexPlots("L1_hfSel_"+trackcoll);
-    vtxp_L1_hf_vtxqualSel             = new VertexPlots("L1_hf_vtxqualSel_"+trackcoll);
+    vtxp_b40Sel                       = new VertexPlots("vtxp_b40Sel_"+trackcoll);
+    vtxp_L1Sel                        = new VertexPlots("vtxp_L1Sel_"+trackcoll);
+    vtxp_hfSel                        = new VertexPlots("vtxp_hfSel_"+trackcoll);
+    vtxp_vtxqualSel                   = new VertexPlots("vtxp_vtxqualSel_"+trackcoll);
+    vtxp_vtxSel                       = new VertexPlots("vtxp_vtxSel_"+trackcoll);
+    vtxp_L1_hfSel                     = new VertexPlots("vtxp_L1_hfSel_"+trackcoll);
+    vtxp_L1_hf_vtxqualSel             = new VertexPlots("vtxp_L1_hf_vtxqualSel_"+trackcoll);
   }
-  vtxp_noSel                          = new VertexPlots("noSel_"+trackcoll);  
-  vtxp_full                           = new VertexPlots("full_"+trackcoll); 
+  vtxp_noSel                          = new VertexPlots("vtxp_noSel_"+trackcoll);  
+  vtxp_full                           = new VertexPlots("vtxp_full_"+trackcoll); 
   
-  mpreco_noSel                        = new MultiPlots("noSel_"+trackcoll); 
-  mpreco_full                         = new MultiPlots("full_"+trackcoll); 
+  mpreco_noSel                        = new MultiPlots("mpreco_noSel_"+trackcoll); 
+  mpreco_full                         = new MultiPlots("mpreco_full_"+trackcoll); 
   
 
 }
@@ -287,7 +287,7 @@ void NCHEvtSelPlots::write(){
     gpp_CentralGen->write();
     
     //Efficiency Plots (only run for particles, not for tracks)
-    if(!trackcoll.Contains("RECO")) makeEffPlots();
+    //if(!trackcoll.Contains("RECO")) makeEffPlots();
   }  
       
   gDirectory->cd("../");
@@ -298,17 +298,17 @@ void NCHEvtSelPlots::makeEffPlots(){
     
 //GenPartPlots
    if(switchIntermedPlots){ 
-        gpp_b40Sel->divide(*gpp_noSel,"b40_"+trackcoll);
-        gpp_L1Sel->divide(*gpp_noSel,"L1_"+trackcoll);
-        gpp_hfSel->divide(*gpp_noSel,"hf_"+trackcoll);
-        gpp_vtxqualSel->divide(*gpp_noSel,"vtxqual_"+trackcoll);
-        gpp_vtxSel->divide(*gpp_noSel,"vtx_"+trackcoll);
-        gpp_L1_hfSel->divide(*gpp_noSel,"L1_hf_"+trackcoll);
-        gpp_L1_hf_vtxqualSel->divide(*gpp_noSel,"L1_hf_vtxqual_"+trackcoll);  
+        gpp_b40Sel->divide(*gpp_noSel,"gpp_b40_"+trackcoll);
+        gpp_L1Sel->divide(*gpp_noSel,"gpp_L1_"+trackcoll);
+        gpp_hfSel->divide(*gpp_noSel,"gpp_hf_"+trackcoll);
+        gpp_vtxqualSel->divide(*gpp_noSel,"gpp_vtxqual_"+trackcoll);
+        gpp_vtxSel->divide(*gpp_noSel,"gpp_vtx_"+trackcoll);
+        gpp_L1_hfSel->divide(*gpp_noSel,"gpp_L1_hf_"+trackcoll);
+        gpp_L1_hf_vtxqualSel->divide(*gpp_noSel,"gpp_L1_hf_vtxqual_"+trackcoll);  
    }     
    //gpp_full->divide(*gpp_noSel,"full_"+trackcoll); 
 //MultiPlots   
-   mppart_full->divide(*mppart_noSel,"partfull_"+trackcoll); 
-   gpp_noSel->divide(*gpp_CentralGen,"CentrEff_"+trackcoll);
+   mppart_full->divide(*mppart_noSel,"mp_partfull_"+trackcoll); 
+   gpp_noSel->divide(*gpp_CentralGen,"gpp_CentrEff_"+trackcoll);
 }
   
