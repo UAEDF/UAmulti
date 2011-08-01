772d0a0b2749ce9fff01768aafe8e8bc755870cc
rougny <>
2011-08-01 11:27:44 +0000

    passed it to new tree structure.Added _highNCH() functions.

--- ChPartAna/macro/FBcuts.C
+++ ChPartAna/macro/FBcuts.C
@@ -1,6 +1,6 @@
 double pt_cut   = 0.15;
 double eta_cut  = 2.4;
-double vtxz_cut = 15.;
+double vtxz_cut = 10.;
 double nsigma   = 4.;
 
 //----------------- GET THE BEST VERTEX ------------------
@@ -15,13 +15,13 @@ int getBestVertex(vector<MyVertex>* vtxcoll){
       if(itvtx->ntracks>ntracks){
         goodVtx  =  itvtx;
 	ntracks  =  itvtx->ntracks;
-	chi2n    =  itvtx->chi2n;
+	chi2n    =  itvtx->chi2n();
       }
       else if(itvtx->ntracks==ntracks){
-        if(chi2n>itvtx->chi2n){
+        if(chi2n>itvtx->chi2n()){
 	  goodVtx  =  itvtx;
 	  ntracks  =  itvtx->ntracks;
-	  chi2n    =  itvtx->chi2n;
+	  chi2n    =  itvtx->chi2n();
 	}
       }
     }
@@ -41,13 +41,13 @@ MyVertex* GetBestVertex(vector<MyVertex>* vtxcoll){
       if(itvtx->ntracks>ntracks){
         goodVtx  =  itvtx;
         ntracks  =  itvtx->ntracks;
-        chi2n    =  itvtx->chi2n;
+        chi2n    =  itvtx->chi2n();
       }
       else if(itvtx->ntracks==ntracks){
-        if(chi2n>itvtx->chi2n){
+        if(chi2n>itvtx->chi2n()){
           goodVtx  =  itvtx;
           ntracks  =  itvtx->ntracks;
-          chi2n    =  itvtx->chi2n;
+          chi2n    =  itvtx->chi2n();
         }
       }
     }
@@ -66,18 +66,16 @@ inline int getVtxposFromId(const MyTracks& tr,int goodvtxId){
   return -1;
 }
  
-
 //--------------------------- Get the Part in the good acceptance -----------------------------
 
-
-inline bool isInAcceptance(const MyPart& p , double pt , double eta , double charge = 0){
-  if(p.v.Pt()<pt) return false;
-  if(fabs(p.v.Eta())>eta) return false;
+template <class T>
+bool isInAcceptance(const T& p , double pt , double eta , double charge = 0){
+  if(p.Pt()<pt) return false;
+  if(fabs(p.Eta())>eta) return false;
   if(charge!=0)
     if(charge!=p.charge)return false;
   return true;
 }
-
 
 
 //------------------ UE SELECTION ----------------------------------------
@@ -89,13 +87,13 @@ bool isTrackPrimary(const MyTracks& tr, int goodvtxId){
   int vtxnum = getVtxposFromId(tr,goodvtxId);
   
   //Make the cuts:
-  //if( tr.Part.v.Pt()<pt_cut ) return false;
-  //if( fabs(tr.Part.v.Eta())>eta_cut) return false;
+  //if( tr.Pt()<pt_cut ) return false;
+  //if( fabs(tr.Eta())>eta_cut) return false;
   
   //--------- PV track cut from Luca -----------
   if( fabs(tr.vtxdxy.at(vtxnum)) / tr.ed0 > 5 ) return false;
   if( fabs(tr.vtxdz.at(vtxnum)) / tr.edz > 5 ) return false;
-  if( tr.ept / tr.Part.v.Pt() > 0.1 ) return false;
+  if( tr.ept / tr.Pt() > 0.1 ) return false;
   
   //To use only with generalTracks, otherwise it's filled to 0 by default
   //if( ! tr.quality[2]) return false;
@@ -124,9 +122,9 @@ bool isTrackPrimary(MyTracks& tr, vector<MyVertex>& vtxcoll, int goodvtxId, MyBe
       goodVtx = it_vtx;
   
   //Make the cuts:
-  //if( tr.Part.v.Pt()<pt_cut ) return false;
-  //if( fabs(tr.Part.v.Eta())>eta_cut) return false;
-  if( tr.ept / tr.Part.v.Pt() > 0.1 ) return false;
+  //if( tr.Pt()<pt_cut ) return false;
+  //if( fabs(tr.Eta())>eta_cut) return false;
+  if( tr.ept / tr.Pt() > 0.1 ) return false;
   
   
   //--------- PV track cut from Ferenc -----------
@@ -138,7 +136,7 @@ bool isTrackPrimary(MyTracks& tr, vector<MyVertex>& vtxcoll, int goodvtxId, MyBe
   
   // Longitudinal impact parameter (4*sigma)
   double sigma_z = sqrt( pow(tr.edz,2) +
-                    pow(cosh(tr.Part.v.Eta()),2) *
+                    pow(cosh(tr.Eta()),2) *
                     bs->BeamWidthX *
                     bs->BeamWidthY);
   //if( fabs(tr.vtxdz.at(vtxnum)) > 4 * sigma_z ) return false; // FIXME
@@ -155,6 +153,18 @@ bool isTrackPrimary(MyTracks& tr, vector<MyVertex>& vtxcoll, int goodvtxId, MyBe
 }
 
 
+bool isTrackPrimary_highNCH(const MyTracks& tr, int goodvtxId , double dzMax = 999){
+
+  int vtxnum = getVtxposFromId(tr,goodvtxId);
+  
+  if( fabs(tr.vtxdxy.at(vtxnum)) / tr.ed0 > 3 ) return false;
+  if( fabs(tr.vtxdz.at(vtxnum)) / tr.edz > 3 ) return false;
+  //if( tr.ept / tr.Pt() > 0.05 ) return false; //0.1
+  if( fabs(tr.vtxdz.at(vtxnum)) > dzMax ) return false;
+  return true;
+
+
+}
 
 
 
@@ -170,7 +180,7 @@ int getnPrimaryTracks(vector<MyTracks>* v_tr , vector<MyVertex>* vtxcoll , doubl
   
   
   for(vector<MyTracks>::iterator it_tr = v_tr->begin() ; it_tr != v_tr->end() ; ++it_tr)
-    if( isTrackPrimary(*it_tr , getBestVertex(vtxcoll)) && isInAcceptance(it_tr->Part,pt,eta,charge) )
+    if( isTrackPrimary(*it_tr , getBestVertex(vtxcoll)) && isInAcceptance(*it_tr,pt,eta,charge) )
       ++nch;
       
   if(debug) cout<<" ** getnPrimaryTracks() has found "<<nch<<" primary tracks"<<endl;
@@ -191,7 +201,7 @@ int getnPrimaryTracks(vector<MyTracks>* v_tr , vector<MyVertex>* vtxcoll, MyBeam
 
 
   for(vector<MyTracks>::iterator it_tr = v_tr->begin() ; it_tr != v_tr->end() ; ++it_tr)
-    if( isTrackPrimary(*it_tr , *vtxcoll , getBestVertex(vtxcoll), bs) && isInAcceptance(it_tr->Part,pt,eta,charge) )
+    if( isTrackPrimary(*it_tr , *vtxcoll , getBestVertex(vtxcoll), bs) && isInAcceptance(*it_tr,pt,eta,charge) )
       ++nch;
 
   if(debug) cout<<" ** getnPrimaryTracks() has found "<<nch<<" primary tracks"<<endl;
@@ -200,12 +210,14 @@ int getnPrimaryTracks(vector<MyTracks>* v_tr , vector<MyVertex>* vtxcoll, MyBeam
 }
 
 
+
+
 //------------------------------ GET TRACKS IN ACCEPTANCE -----------------------------------------
 template <class T>
 vector<T> getInAcc(vector<T> v , double pt = pt_cut, double eta = eta_cut,double charge = 0.){
     
   for(typename vector<T>::iterator it = v.end()-1 ; it != v.begin()-1 ; --it)
-    if( !isInAcceptance(it->Part,pt,eta,charge) )  v.erase(it);
+    if( !isInAcceptance(it,pt,eta,charge) )  v.erase(it);
 
   return v;
 
@@ -218,7 +230,7 @@ int getnInAcc(vector<T>* v , double pt = pt_cut, double eta = eta_cut, double ch
 
   int nch=0;
   for(typename vector<T>::iterator it = v->begin() ; it != v->end() ; ++it)
-    if( isInAcceptance(it->Part,pt,eta,charge) ) ++nch;
+    if( isInAcceptance(*it,pt,eta,charge) ) ++nch;
 
   return nch;
 
@@ -234,7 +246,7 @@ void getPrimaryTracks(vector<MyTracks>& v_tr , int vtxId , double pt = pt_cut, d
   
   
   for(vector<MyTracks>::iterator it_tr = v_tr.end()-1 ; it_tr != v_tr.begin()-1 ; --it_tr)
-    if( ! isTrackPrimary(*it_tr , vtxId) || !isInAcceptance(it_tr->Part,pt,eta,charge) || (highPurityOnly && !it_tr->quality[2]) )
+    if( ! isTrackPrimary(*it_tr , vtxId) || !isInAcceptance(*it_tr,pt,eta,charge) || (highPurityOnly && !it_tr->quality[2]) )
       v_tr.erase(it_tr);
       
   if(debug) cout<<" ** "<<v_tr.size()<<" tracks remaining after primary selection"<<endl;
@@ -246,6 +258,36 @@ inline void getPrimaryTracks(vector<MyTracks>& v_tr , vector<MyVertex>* vtxcoll 
 
 
 
+//------------------------------ GET THE PRIMARY gTR highNCH -----------------------------------------
+void getPrimaryTracks_highNCH(vector<MyTracks>& v_tr , int vtxId , double pt = pt_cut, double eta = eta_cut,
+                   double charge = 0, bool highPurityOnly = false , double dzMax = 999){
+
+  if(debug) cout<<" +-+-+ starting getPrimaryTracks"<<endl;
+  
+  //Protecting code if no vertex were found
+  if(vtxId==-1) v_tr.clear();
+
+   
+  for(vector<MyTracks>::iterator it_tr = v_tr.end()-1 ; it_tr != v_tr.begin()-1 ; --it_tr){
+    /*int vtxnum = getVtxposFromId(*it_tr,vtxId);
+    cout << it_tr->Pt() << "  "
+         << it_tr->Eta() << "  "
+         << it_tr->vtxdz.at(vtxnum) << "  "
+         << fabs(it_tr->vtxdxy.at(vtxnum)) / it_tr->ed0 << "  "
+         << fabs(it_tr->vtxdz.at(vtxnum)) / it_tr->edz << endl;
+	 
+	 
+  
+    cout << isTrackPrimary_highNCH(*it_tr , vtxId , dzMax) << "  " 
+         << isInAcceptance(*it_tr,pt,eta,charge) << "  " << (highPurityOnly && (!it_tr->quality[2]))  << endl;*/
+    if( (!isTrackPrimary_highNCH(*it_tr , vtxId , dzMax)) || (!isInAcceptance(*it_tr,pt,eta,charge)) || ((highPurityOnly && !it_tr->quality[2])) )
+      v_tr.erase(it_tr);
+  }
+      
+  if(debug) cout<<" ** "<<v_tr.size()<<" tracks remaining after primary selection"<<endl;
+}
+
+
 //------------------------------ GET THE PRIMARY mbTR -----------------------------------------
 void getPrimaryTracks(vector<MyTracks>& v_tr , vector<MyVertex>* vtxcoll, int vtxId, MyBeamSpot* bs, double pt = pt_cut, double eta = eta_cut, double charge = 0){
 
@@ -255,7 +297,7 @@ void getPrimaryTracks(vector<MyTracks>& v_tr , vector<MyVertex>* vtxcoll, int vt
   if(vtxId==-1) v_tr.clear();
   
   for(vector<MyTracks>::iterator it_tr = v_tr.end()-1 ; it_tr != v_tr.begin()-1 ; --it_tr)
-    if( ! isTrackPrimary(*it_tr , *vtxcoll, vtxId, bs ) || !isInAcceptance(it_tr->Part,pt,eta,charge) )
+    if( ! isTrackPrimary(*it_tr , *vtxcoll, vtxId, bs ) || !isInAcceptance(*it_tr,pt,eta,charge) )
        v_tr.erase(it_tr);
       
   if(debug) cout<<" ** "<<v_tr.size()<<" tracks remaining after primary selection"<<endl;
@@ -269,7 +311,7 @@ inline void getPrimaryTracks(vector<MyTracks>& v_tr , vector<MyVertex>* vtxcoll,
 
 //--------------------------- IS THE GenPart GOOD ------------------
 bool isGenPartGood(const MyGenPart& p){
-  if ( fabs(p.Part.charge) <=0 ) return false;
+  if ( fabs(p.charge) <=0 ) return false;
   if ( p.status != 1 )          return false;
 
   //Count only Stable Hadrons (and not the leptons) *** NOT LONGER NEEDED FOR MBUEWG
@@ -287,7 +329,7 @@ int getnPrimaryGenPart(vector<MyGenPart>* v_tr , double pt = pt_cut, double eta 
   int nch = 0;
   
   for(vector<MyGenPart>::iterator it_tr = v_tr->begin() ; it_tr != v_tr->end() ; ++it_tr)
-    if( isGenPartGood(*it_tr) && isInAcceptance(it_tr->Part,pt,eta,charge) )
+    if( isGenPartGood(*it_tr) && isInAcceptance(*it_tr,pt,eta,charge) )
       ++nch;
       
   if(debug) cout<<" ** getnPrimaryTracks() has found "<<nch<<" primary tracks"<<endl;
@@ -299,7 +341,7 @@ int getnPrimaryGenPart(vector<MyGenPart>* v_tr , double pt = pt_cut, double eta 
 void getPrimaryGenPart(vector<MyGenPart>& v_tr , double pt = pt_cut, double eta = eta_cut, double charge = 0){
   
   for(vector<MyGenPart>::iterator it_tr = v_tr.end()-1 ; it_tr != v_tr.begin()-1 ; --it_tr)
-    if( !(isGenPartGood(*it_tr) && isInAcceptance(it_tr->Part,pt,eta,charge)) )
+    if( !(isGenPartGood(*it_tr) && isInAcceptance(*it_tr,pt,eta,charge)) )
       v_tr.erase(it_tr);
   
   if(debug) cout<<" ** "<<v_tr.size()<<" GenPart remaining after primary selection"<<endl;
@@ -317,8 +359,8 @@ template <class T>
 pair<Double_t,Double_t> getnCorrel(vector<T>& v_tr , FBacc& cut){
   Int_t nTrM = 0 , nTrP = 0;
   for(typename vector<T>::iterator it_tr = v_tr.begin() ; it_tr != v_tr.end() ; ++it_tr){
-    if( it_tr->Part.v.Eta() < cut.etaM && it_tr->Part.v.Eta() > cut.etaM - cut.widthM ) nTrM++;
-    if( it_tr->Part.v.Eta() > cut.etaP && it_tr->Part.v.Eta() < cut.etaP + cut.widthP ) nTrP++;
+    if( it_tr->Eta() < cut.etaM && it_tr->Eta() > cut.etaM - cut.widthM ) nTrM++;
+    if( it_tr->Eta() > cut.etaP && it_tr->Eta() < cut.etaP + cut.widthP ) nTrP++;
   }
   if(debug) cout << "[getnCorrel] ntrM , nTrP : " << nTrM << "  " << nTrP << endl;
   return make_pair(nTrM , nTrP);
@@ -331,8 +373,8 @@ pair<Double_t,Double_t> getnCorrelTrReweight(vector<T>& v_tr , FBacc& cut , TH2F
   Int_t xbin = 0 , ybin = 0;
   Double_t weight = 1 , trEff_val = 1 , trFakes_val = 1;
   for(typename vector<T>::iterator it_tr = v_tr.begin() ; it_tr != v_tr.end() ; ++it_tr){
-    xbin = trEff.GetXaxis()->FindFixBin( it_tr->Part.v.Eta() ) ;
-    ybin = trEff.GetYaxis()->FindFixBin( it_tr->Part.v.Pt()  ) ;
+    xbin = trEff.GetXaxis()->FindFixBin( it_tr->Eta() ) ;
+    ybin = trEff.GetYaxis()->FindFixBin( it_tr->Pt()  ) ;
     trEff_val = trEff.GetBinContent( xbin , ybin );
     trFakes_val =  1. - trFakes.GetBinContent( xbin , ybin );
     if(&trFakes==0) trFakes_val = 1;
@@ -342,11 +384,11 @@ pair<Double_t,Double_t> getnCorrelTrReweight(vector<T>& v_tr , FBacc& cut , TH2F
 
     if(debug && weight > 2){
       cout << "[getnCorrelTrReweight] trEff_val , trFakes_val , weight : " << trEff_val << "  " << trFakes_val << "  " << weight << endl;
-      cout << "[getnCorrelTrReweight] pt , eta                         : " << it_tr->Part.v.Pt() << "  " << it_tr->Part.v.Eta() << endl;
+      cout << "[getnCorrelTrReweight] pt , eta                         : " << it_tr->Pt() << "  " << it_tr->Eta() << endl;
     }
 
-    if( it_tr->Part.v.Eta() < cut.etaM && it_tr->Part.v.Eta() > cut.etaM - cut.widthM ) nTrM+=weight;
-    if( it_tr->Part.v.Eta() > cut.etaP && it_tr->Part.v.Eta() < cut.etaP + cut.widthP ) nTrP+=weight;
+    if( it_tr->Eta() < cut.etaM && it_tr->Eta() > cut.etaM - cut.widthM ) nTrM+=weight;
+    if( it_tr->Eta() > cut.etaP && it_tr->Eta() < cut.etaP + cut.widthP ) nTrP+=weight;
   }
   if(debug) cout << "[getnCorrelTrReweight] ntrM , nTrP : " << nTrM << "  " << nTrP << endl;
   return make_pair(nTrM , nTrP);
