710205652d30104e2ebf7034ee2e508370797a99
rougny <>
2011-08-01 11:52:32 +0000

    need separate one in order to use in other classes.

---
+++ ChPartAna/plugins/toperationLinkDef.C
@@ -0,0 +1,16 @@
+#include "./TOperation.h"
+#include "TH1F.h"
+#include "TH2F.h"
+
+#ifdef __MAKECINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+//typedef TH1F* pTH1F;
+#pragma link C++ class TOperation<TH1F>+;
+#pragma link C++ class TOperation<TH2F>+;
+#pragma link C++ class vector<TOperation<TH1F> >+;
+#pragma link C++ class vector<TOperation<TH2F> >+;
+
+#endif
