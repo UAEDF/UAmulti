0808b07166d74208bc77e8cdfd5e930301c5b0a3
rougny <>
2012-01-20 13:32:26 +0000

    add TProfile

--- ChPartAna/plugins/toperationLinkDef.C
+++ ChPartAna/plugins/toperationLinkDef.C
@@ -1,6 +1,7 @@
 #include "./TOperation.h"
 #include "TH1F.h"
 #include "TH2F.h"
+#include "TProfile.h"
 
 #ifdef __MAKECINT__
 #pragma link off all globals;
@@ -10,7 +11,9 @@
 //typedef TH1F* pTH1F;
 #pragma link C++ class TOperation<TH1F>+;
 #pragma link C++ class TOperation<TH2F>+;
+#pragma link C++ class TOperation<TProfile>+;
 #pragma link C++ class vector<TOperation<TH1F> >+;
 #pragma link C++ class vector<TOperation<TH2F> >+;
+#pragma link C++ class vector<TOperation<TProfile> >+;
 
 #endif
